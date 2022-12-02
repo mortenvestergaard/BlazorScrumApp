1300f85e53d93a84132c58f3f29a3ba4802bdef4
Morten Vestergaard <mbvestergaard@hotmail.com>
2022-12-02 11:08:24 +0100

    new ngrok

--- Services/ScrumboardService.cs
+++ Services/ScrumboardService.cs
@@ -15,59 +15,59 @@ namespace BlazorScrumApp.Services
 
 		public async Task<List<Models.ScrumTask>> GetTasks()
 		{
-			var tasks = await _client.GetFromJsonAsync<List<Models.ScrumTask>>("https://eaa0-93-176-82-57.eu.ngrok.io/api/Task/GetTasks");
+			var tasks = await _client.GetFromJsonAsync<List<Models.ScrumTask>>("https://7541-93-176-82-57.eu.ngrok.io/api/Task/GetTasks");
 			return tasks;
 		}
 
 		public async Task<List<State>> GetStates()
 		{
-			var states = await _client.GetFromJsonAsync<List<State>>("https://eaa0-93-176-82-57.eu.ngrok.io/api/State/GetStates");
+			var states = await _client.GetFromJsonAsync<List<State>>("https://7541-93-176-82-57.eu.ngrok.io/api/State/GetStates");
 			return states;
 		}
 
 		public async Task<List<User>> GetUsers()
 		{
-			var users = await _client.GetFromJsonAsync<List<User>>("https://eaa0-93-176-82-57.eu.ngrok.io/api/User/GetUsers");
+			var users = await _client.GetFromJsonAsync<List<User>>("https://7541-93-176-82-57.eu.ngrok.io/api/User/GetUsers");
 			return users;
 		}
 
 		public async Task<State> CreateState(State newState)
 		{
-			var returnedState = await _client.PostAsJsonAsync<State>("https://eaa0-93-176-82-57.eu.ngrok.io/api/State/CreateState", newState);
+			var returnedState = await _client.PostAsJsonAsync<State>("https://7541-93-176-82-57.eu.ngrok.io/api/State/CreateState", newState);
 			var data = await returnedState.Content.ReadFromJsonAsync<State>();
 			return data;
 		}
 
 		public async Task<ScrumTask> CreateTask(ScrumTask newTask)
 		{
-			var returnedTask = await _client.PostAsJsonAsync<ScrumTask>("https://eaa0-93-176-82-57.eu.ngrok.io/api/Task/CreateTask", newTask);
+			var returnedTask = await _client.PostAsJsonAsync<ScrumTask>("https://7541-93-176-82-57.eu.ngrok.io/api/Task/CreateTask", newTask);
 			var data = await returnedTask.Content.ReadFromJsonAsync<ScrumTask>();
 			return data;
 		}
 
 		public async Task<ScrumTask> UpdateTaskState(ScrumTask taskToUpdate)
 		{
-			var updatedTask = await _client.PutAsJsonAsync<ScrumTask>("https://eaa0-93-176-82-57.eu.ngrok.io/api/Task/UpdateTaskState", taskToUpdate);
+			var updatedTask = await _client.PutAsJsonAsync<ScrumTask>("https://7541-93-176-82-57.eu.ngrok.io/api/Task/UpdateTaskState", taskToUpdate);
 			var data = await updatedTask.Content.ReadFromJsonAsync<ScrumTask>();
 			return data;
 		}
 
 		public async Task<ScrumTask> UpdateTask(ScrumTask taskToUpdate)
 		{
-			var updatedTask = await _client.PutAsJsonAsync<ScrumTask>("https://eaa0-93-176-82-57.eu.ngrok.io/api/Task/UpdateTask", taskToUpdate);
+			var updatedTask = await _client.PutAsJsonAsync<ScrumTask>("https://7541-93-176-82-57.eu.ngrok.io/api/Task/UpdateTask", taskToUpdate);
 			var data = await updatedTask.Content.ReadFromJsonAsync<ScrumTask>();
 			return data;
 		}
 
         public async Task<HttpResponseMessage> DeleteTask(ScrumTask task)
         {
-			var response = await _client.PostAsJsonAsync("https://eaa0-93-176-82-57.eu.ngrok.io/api/Task/DeleteTask", task);
+			var response = await _client.PostAsJsonAsync("https://7541-93-176-82-57.eu.ngrok.io/api/Task/DeleteTask", task);
             return response;
         }
 
 		public async Task<HttpResponseMessage> DeleteState(State state)
 		{
-			var response = await _client.PostAsJsonAsync("https://eaa0-93-176-82-57.eu.ngrok.io/api/State/DeleteState", state);
+			var response = await _client.PostAsJsonAsync("https://7541-93-176-82-57.eu.ngrok.io/api/State/DeleteState", state);
 			return response;
 		}
 	}
